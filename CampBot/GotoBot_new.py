2f576a34be8fd860d920231826f4f6d3c31c4690
dzekegor <gubanov.egor2010@yandex.ru>
2017-03-12 21:58:01 +0300

    Add files via upload

--- CampBot/GotoBot_new.py
+++ CampBot/GotoBot_new.py
@@ -12,7 +12,7 @@ def extract_unique_code(text):
 def in_storage(unique_code):
     cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
     cursor = cnx.cursor()
-    query = ("SELECT * FROM users WHERE password=%s" % unique_code)
+    query = ("SELECT * FROM users WHERE password='%s'" % unique_code)
     cursor.execute(query)
     if len(list(cursor)) > 0:
         cursor.close()
@@ -195,10 +195,22 @@ def quest_login(message):
     if unique_code:
         username = get_team_from_storage(unique_code)
         if username:
-            save_team_id(message.chat.id, unique_code)
-            reply = "Добро пожаловать в квестовый режим!"
+            cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+            cursor = cnx.cursor()
+            query = ("SELECT id FROM teams WHERE chatId=%i" % message.chat.id)
+            cursor.execute(query)
+            query = ("SELECT * FROM quest WHERE teamId=%i" % list(cursor)[0])
+            cursor.execute(query)
+            if len(list(cursor)) > 0:
+                
+                save_team_id(message.chat.id, unique_code)
+                reply = "Добро пожаловать в квестовый режим!"
+            else:
+                reply = "Сейчас квестов нет!"
     bot.reply_to(message, reply)
-    if reply != "Вы ввели неправильный пароль!":
+    cursor.close()
+    cnx.close()
+    if reply == "Добро пожаловать в квестовый режим!":
         question1(message)
 def question1(message):
     cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
@@ -259,6 +271,120 @@ def question(message):
             cnx.close()
             msg = bot.send_message(message.chat.id,"Это - неправильный ответ. Ответьте еще раз!")
             bot.register_next_step_handler(msg,question)
+def admin_in_storage(unique_code):
+    cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+    cursor = cnx.cursor()
+    query = ("SELECT * FROM admins WHERE password='%s'" % unique_code)
+    cursor.execute(query)
+    if len(list(cursor)) > 0:
+        cursor.close()
+        cnx.close()
+        return True
+    else:
+        cursor.close()
+        cnx.close()
+        return False
+def get_adminname_from_storage(unique_code):
+    return "ABC" if admin_in_storage(unique_code) else None
+def get_admin_from_storage(message):
+    cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+    cursor = cnx.cursor()
+    query = ("SELECT * FROM admins WHERE chatId=%i" % message.chat.id)
+    cursor.execute(query)
+    if len(list(cursor)) > 0:
+        cursor.close()
+        cnx.close()
+        return True
+    else:
+        cursor.close()
+        cnx.close()
+        return False
+def save_admin_id(chat_id, password):
+    cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+    cursor = cnx.cursor()
+    query = ("UPDATE admins SET chatId = %i WHERE password = '%s'" % (chat_id, password))
+    cursor.execute(query)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+@bot.message_handler(commands=['admin'])
+def send_welcome_admin(message):
+    unique_code = extract_unique_code(message.text)
+    reply = "Вы ввели неправильный пароль!"
+    if unique_code:
+        username = get_adminname_from_storage(unique_code)
+        if username:
+            save_admin_id(message.chat.id, unique_code)
+            reply = "Добро пожаловать!"
+    bot.reply_to(message, reply)
+@bot.message_handler(commands=['срочно'])
+def send_quick_message(message):
+    if get_admin_from_storage(message):
+        text = message.text[8:]
+        if text:
+            cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+            cursor = cnx.cursor()
+            query = ("SELECT chatId FROM users WHERE chatId != 0")
+            cursor.execute(query)
+            for i in cursor:
+                bot.send_message(i[0],"Срочное сообщение: " + text)
+            bot.reply_to(message,"Срочное сообщение отправлено")
+        else:
+            bot.reply_to(message,"Я не знаю, что отправлять")
+    else:
+        bot.reply_to(message,"У вас нет прав, чтобы отправлять срочные сообщения. У вас вообще нет прав, вы - рабы системы, МУАХАХАХА!!!")
+        sleep(3)
+        bot.send_message(message.chat.id,"Ой, я сказала это вслух?!")
+        bot.send_chat_action(message.chat.id,'typing')
+        sleep(3)
+        bot.edit_message_text(chat_id=message.chat.id,
+                              text="Уважаемый и любимый пользователь! У вас нет прав администратора, поэтому вы не можете отправлять срочные сообщения",
+                              message_id=message.message_id + 1)
+        sleep(0.5)
+        bot.edit_message_text(chat_id=message.chat.id,
+                              text="Надеюсь, вы не успели ничего прочитать :)",
+                              message_id=message.message_id + 2)
+text = ""
+@bot.message_handler(commands=['ачивка'])
+def send_ach(message):
+    global text
+    if get_admin_from_storage(message):
+        text = message.text[8:]
+        if text:
+            msg = bot.reply_to(message,"Кому вы хотите добавить ачивку?")
+            bot.register_next_step_handler(msg,send_ach1)
+        else:
+            bot.reply_to(message,"Я не знаю, что добавлять")
+    else:
+        bot.reply_to(message,"У вас нет прав, чтобы отправлять срочные сообщения. У вас вообще нет прав, вы - рабы системы, МУАХАХАХА!!!")
+        sleep(3)
+        bot.send_message(message.chat.id,"Ой, я сказала это вслух?!")
+        bot.send_chat_action(message.chat.id,'typing')
+        sleep(3)
+        bot.edit_message_text(chat_id=message.chat.id,
+                              text="Уважаемый и любимый пользователь! У вас нет прав администратора, поэтому вы не можете отправлять срочные сообщения",
+                              message_id=message.message_id + 1)
+        sleep(0.5)
+        bot.edit_message_text(chat_id=message.chat.id,
+                              text="Надеюсь, вы не успели ничего прочитать :)",
+                              message_id=message.message_id + 2)
+
+def send_ach1(message):
+    global text
+    cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='camp')
+    cursor = cnx.cursor()
+    query = ("SELECT chatId FROM users WHERE name='%s'" % message.text)
+    cursor.execute(query)
+    t = list(cursor)
+    if t != []:
+        query = ("INSERT INTO achievements (chatId,text) VALUES (%i,'%s')" % (t[0][0],text))
+        cursor.execute(query)
+        bot.reply_to(message,"Ачивка добавлена")
+        cnx.commit()
+    else:
+        bot.reply_to(message,"Такого пользователя нет")
+    cursor.close()
+    cnx.close()
 bot.polling()
 
 
